75e03c7a6cce57e7cf57ce16e9b7b4cfa6bcc29b
Grigory98 <yakimov_ga@mail.ru>
2022-06-25 19:14:44 +0400

    new pipeline

---
+++ test.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    
+    environment {
+        WORK_FOLDER = 'C:\\TEST_JENKINS\\'
+    }
+    
+    stages {
+        stage('Begin') {
+            steps {
+                echo 'Begin job'
+            }
+        }
+        stage('Ping') {
+            steps {
+                bat "git clone https://github.com/Grigory98/tests-course.git ${WORK_FOLDER}"
+            }
+        }
+        stage('End')
+        {
+            steps {
+                echo 'End job'
+            }
+        }
+    }
+}
