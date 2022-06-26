74c9dfbc5127404c7047c8c68003599381b72348
Grigory98 <yakimov_ga@mail.ru>
2022-06-26 20:05:00 +0400

    add new command

--- test.groovy
+++ test.groovy
@@ -5,17 +5,18 @@ pipeline {
 
     environment {
         WORK_FOLDER = 'C:\\TEST_JENKINS\\'
+        GIT_FOLDER  = 'C:\\Program Files\\Git\\bin'
     }
     
     stages {
         stage('Begin') {
             steps {
-                echo 'Begin job, test 3'
+                echo 'Begin job, test 4'
             }
         }
         stage('Clone project') {
             steps {
-                bat "ping yandex.ru"
+                bat "cd ${GIT_FOLDER} && git clone https://github.com/Grigory98/tests-course.git ${WORK_FOLDER}"
                 //bat "git clone https://github.com/Grigory98/tests-course.git ${WORK_FOLDER}"
             }
         }
