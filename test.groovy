6cf12f413290dbf067ae3d9c560c44ae9cdcacdb
Grigory98 <yakimov_ga@mail.ru>
2022-06-26 20:38:50 +0400

    test again

--- test.groovy
+++ test.groovy
@@ -15,7 +15,7 @@ pipeline {
             }
         }
         stage('Clone project') {
-            when { not { expression { return fileExists('README.md') } } }
+            when { expression { return !fileExists('README.md') } }
             steps {
                 echo "Clone git project into local directory: ${GIT_FOLDER}"
                 bat "cd ${GIT_FOLDER} && git clone https://github.com/Grigory98/tests-course.git ${WORK_FOLDER}"
