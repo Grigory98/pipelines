228cadc3e6ca582f6ab47811eb0c9ca38d6f13c0
Grigory98 <yakimov_ga@mail.ru>
2022-06-28 19:35:32 +0400

    test

--- test.groovy
+++ test.groovy
@@ -19,7 +19,7 @@ pipeline {
             when { expression { MY_FILE == 'true'} }
             steps {
                 echo "Pull git project into local directory: ${GIT_FOLDER}"
-                bat "cd ${GIT_FOLDER} && git pull ${WORK_FOLDER}"
+                bat "cd ${MY_FILE} && git pull ${WORK_FOLDER}"
             }
         }
         stage('Clone project') {
