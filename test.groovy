e25f415e741254b28f064ba2759233b376a5b850
Grigory98 <yakimov_ga@mail.ru>
2022-06-28 20:17:23 +0400

    test

--- test.groovy
+++ test.groovy
@@ -7,19 +7,22 @@ pipeline {
         WORK_FOLDER = 'C:\\TEST_JENKINS\\'
         GIT_FOLDER  = 'C:\\Program Files\\Git\\bin'
         MY_FILE = fileExists 'C:\\TEST_JENKINS\\README.md'
+        DISABLE_AUTH = 'true'
+        ENABLE_AUTH = 'false'
     }
     
     stages {
         stage('Begin') {
             steps {
                 echo 'Starting job...'
+                
             }
         }
         stage('Pull project') {
             when { expression { MY_FILE == 'true'} }
             steps {
                 echo "Pull git project into local directory: ${GIT_FOLDER}"
-                bat "cd ${WORK_FOLDER} && ${GIT_FOLDER}\\git.exe git pull"
+                bat "cd ${WORK_FOLDER} && cd ${GIT_FOLDER}\\git.exe && git pull"
             }
         }
         stage('Clone project') {
